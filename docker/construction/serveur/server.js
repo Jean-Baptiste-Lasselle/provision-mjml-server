02e639cc943ec2605c16eab49eb1762759d44fef
Jean-Baptiste-Lasselle <35227860+Jean-Baptiste-Lasselle@users.noreply.github.com>
2019-03-19 06:55:13 +0100

    Update server.js

--- docker/construction/serveur/server.js
+++ docker/construction/serveur/server.js
@@ -1,6 +1,18 @@
 
 var express = require("express");
 
-var app = express();
 
+
+
+const hostname = process.env.HOST;
+const numeroPortMonAppli = process.env.PORT;
+
+
+var app = express();
 app.use(express.static('grapesjs'));
+
+var server = app.listen(numeroPortMonAppli, hostname, function(){
+    var numport = server.address().port;
+    var hostname = server.address();
+    console.log("Server started at http://" + hostname + ":%s", numport);
+});
