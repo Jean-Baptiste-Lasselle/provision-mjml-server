e3e55de96cd0899afb139d1c9be56a8ca21719f1
Jean-Baptiste-Lasselle <35227860+Jean-Baptiste-Lasselle@users.noreply.github.com>
2019-03-19 04:38:08 +0100

    Update index.js

--- docker/construction/serveur/index.js
+++ docker/construction/serveur/index.js
@@ -5,8 +5,18 @@
 
 const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+/**
+ * Just do it : https://nodejs.org/en/docs/guides/getting-started-guide/
+ *  Il faut donc démarrer l'application avec : 
+ *            $ PORT=4589 NODE_ENV=development node index.js
+ * 
+ * Pour un Dockerfile, idéalement la partie ENTRYPOINT ["PORT", "=", "4589", "NODE_ENV", "=", "development"] 
+ * 
+ **/
+
+// const hostname = '127.0.0.1';
+const hostname = process.env.HOST;
+const numeroPortMonAppli = process.env.PORT;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -15,5 +25,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, hostname, () => {
-  console.log(`MJML editor server is now running at http://${hostname}:${port}/`);
+  console.log(`MJML editor server is now running at http://${hostname}:${numeroPortMonAppli}/`);
 });
